a427bb29ac6df6373f3088dc0f3ed37e82cc52d2
JiuRanYa <1084350607@qq.com>
2024-11-12 11:44:54 +0800

    fix: button focus outline

--- src/layout/main.tsx
+++ src/layout/main.tsx
@@ -171,11 +171,11 @@ export default () => {
         </div>
 
         {/* Menu Button */}
-        <Menu as="div" className="relative">
-          <Menu.Button className="inline-flex items-center gap-2 px-3 py-1.5 text-sm hover:bg-black/5 rounded-md">
+        <Menu as="div" className="relative focus:outline-none outline-none">
+          <MenuButton className="inline-flex items-center gap-2 px-3 py-1.5 text-sm hover:bg-black/5 rounded-md outline-none">
             <span className="mr-2">{currentWorkspace.name}</span>
             <HamburgerIcon className="h-4 w-4" />
-          </Menu.Button>
+          </MenuButton>
 
           <Transition
             as={Fragment}
