d0b6ae33a633f7f55bddde52662149a855cd5d5a
JiuRanYa <1084350607@qq.com>
2024-11-13 18:30:38 +0800

    chore: eslint code lint

--- src/hooks/useLanguage.ts
+++ src/hooks/useLanguage.ts
@@ -1,21 +1,21 @@
-import { useTranslation } from 'react-i18next';
 import { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export const useLanguage = () => {
-    const { i18n } = useTranslation();
-    const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
+  const { i18n } = useTranslation();
+  const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
 
-    const changeLanguage = (lng: string) => {
-        i18n.changeLanguage(lng);
-        setCurrentLanguage(lng);
-    };
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+    setCurrentLanguage(lng);
+  };
 
-    useEffect(() => {
-        setCurrentLanguage(i18n.language);
-    }, [i18n.language]);
+  useEffect(() => {
+    setCurrentLanguage(i18n.language);
+  }, [i18n.language]);
 
-    return {
-        currentLanguage,
-        changeLanguage,
-    };
+  return {
+    currentLanguage,
+    changeLanguage,
+  };
 }; 
